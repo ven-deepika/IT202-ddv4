1a450b2d56f848e0dd48d4ee0a9f67a52e809f14
username <MY_NAME@example.com>
2024-02-08 19:31:40 -0500

    delete m1 files

--- M1/lib/sanitizers.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function sanitize_email($email = "") {
-    return filter_var(trim($email), FILTER_SANITIZE_EMAIL);
-}
-function is_valid_email($email = "") {
-    return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
-}
